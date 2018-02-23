39a0aadfafb0f59eaaf88adbf9332a1bdbbf9139
luowk <lopp1100@163.com>
2018-02-23 16:37:27 +0800

    add backend image upload and richtext image upload

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -1,20 +1,27 @@
 package com.mmall.controller.backend;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
-import com.mmall.dao.ProductMapper;
 import com.mmall.pojo.Product;
 import com.mmall.pojo.User;
+import com.mmall.service.FileService;
 import com.mmall.service.ProductService;
 import com.mmall.service.UserService;
+import com.mmall.util.PropertiesUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/manage/product")
@@ -25,6 +32,9 @@ public class ProductManageController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private FileService fileService;
 
     @RequestMapping(value = "/save.do", method = RequestMethod.POST)
     @ResponseBody
@@ -97,6 +107,69 @@ public class ProductManageController {
         } else {
             return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
         }
+    }
+
+    @RequestMapping(value = "/upload.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse upload(HttpSession session, @RequestParam(value = "uploadFile", required = false) MultipartFile file,
+                                 HttpServletRequest request) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorMessage("用户未登陆，请登陆");
+        }
+        if (userService.checkAdminRole(user).isSuccess()) {
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = fileService.upload(file, path);
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+            Map fileMap = Maps.newHashMap();
+            fileMap.put("uri", targetFileName);
+            fileMap.put("url", url);
+            return ServerResponse.createBySuccess(fileMap);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
+        }
+
+    }
+
+    @RequestMapping(value = "/richtextImgUpload.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Map richtextImgUpload(HttpSession session, @RequestParam(value = "uploadFile", required = false) MultipartFile file,
+                                 HttpServletRequest request, HttpServletResponse response) {
+        Map resultMap = Maps.newHashMap();
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            resultMap.put("success", false);
+            resultMap.put("msg", "用户未登陆，请登陆");
+            return resultMap;
+        }
+        //富文本中对于返回值有自己的要求，我们使用的是simditor的要求进行返回
+//        {
+//            "success": true/false,
+//                "msg": "error message", # optional
+//            "file_path": "[real file path]"
+//        }
+        if (userService.checkAdminRole(user).isSuccess()) {
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = fileService.upload(file, path);
+            if (StringUtils.isBlank(targetFileName)) {
+                resultMap.put("success", false);
+                resultMap.put("msg", "上传失败");
+                return resultMap;
+            }
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+            resultMap.put("success", true);
+            resultMap.put("msg", "上传成功");
+            resultMap.put("file_path", url);
+            response.addHeader("Access-Control-Allow-Headers", "X-File-Name");
+            return resultMap;
+        } else {
+            resultMap.put("success", false);
+            resultMap.put("msg", "\"无权限操作，需要管理员权限\"");
+            return resultMap;
+        }
+
     }
 
 }
